af8708a34e1653def7f150314337f8222cf7a511
maikuvit <michelevitale000@gmail.com>
2021-10-28 09:36:49 +0200

    working test

---
+++ src/test/java/it/maiku/myListUtilTest.java
@@ -0,0 +1,47 @@
+package it.maiku;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+@RunWith(value = Parameterized.class)
+public class myListUtilTest {
+
+    int order = 0;
+    ArrayList<Integer> data;
+
+    @Parameters
+    public static Iterable<Object[]> testData(){
+        return Arrays.asList(new Object[][]{
+                {0, new ArrayList<Integer>( Arrays.asList(1, 2 ,6, 0, 5)) },
+                {1, new ArrayList<Integer>( Arrays.asList(1, 2 ,6, 0, 5))},
+                {0, new ArrayList<Integer>()},
+                {1, new ArrayList<Integer>( Arrays.asList(0, 0 ,1, 0, 0))},
+
+        });
+    }
+
+    public myListUtilTest(int inpOrder, ArrayList<Integer> inpData) {
+        order = inpOrder;
+        data = inpData;
+    }
+
+    @Test
+    public void completeTest()
+    {
+        //ordino dataset su lista di appoggio ...
+        ArrayList<Integer> ordData = new ArrayList<Integer>(data);
+        if(order == 0) Collections.sort(ordData);
+        else Collections.sort(ordData,Collections.reverseOrder());
+
+        //check sul testcase ...
+        Assert.assertTrue(myListUtil.sort(data,order).equals(ordData));
+    }
+}
